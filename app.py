cda2b32f9996fa30a28c69a510986247a91d6e87
Tiago Camargo Maciel dos Santos <tagocms@gmail.com>
2024-08-15 21:57:06 -0300

    changes 15.08

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 from flask import Flask, render_template, redirect
-import flask_session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    return render_template("signup.html")
